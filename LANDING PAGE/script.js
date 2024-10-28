bdba5f625d8e5f60781cf5ec3223f2991eca014f
KrishBahukhandi <krishbahukhandi35@gmail.com>
2024-10-28 10:48:02 +0530

    all pages are linked

--- LANDING PAGE/script.js
+++ LANDING PAGE/script.js
@@ -2,3 +2,18 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('active');
 });
+
+const toggleButton = document.getElementById('theme-toggle');
+    const themeIcon = document.getElementById('theme-icon');
+
+    toggleButton.addEventListener('click', function () {
+        document.body.classList.toggle('light-mode');
+
+        if (document.body.classList.contains('light-mode')) {
+            themeIcon.classList.remove('fa-sun');
+            themeIcon.classList.add('fa-moon');
+        } else {
+            themeIcon.classList.remove('fa-moon');
+            themeIcon.classList.add('fa-sun');
+        }
+    });
